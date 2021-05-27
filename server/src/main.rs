24dd843e35bd1caa1ee9b546f84d3f3bc1ee79db
Lars Francke <lars.francke@gmail.com>
2021-05-27 09:14:37 +0200

    Add a comment

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,6 +4,7 @@ use stackable_operator::{client, error};
 async fn main() -> Result<(), error::Error> {
     stackable_operator::initialize_logging("HDFS_OPERATOR_LOG");
 
+    info!("Starting Stackable Operator for Apache Hadoop HDFS");
     let client = client::create_client(Some("hdfs.stackable.tech".to_string())).await?;
     stackable_hdfs_operator::create_controller(client).await;
     Ok(())
