3db0d518c20a46a12dac042dd1bf0b5da5a4e91a
Andrew Kenworthy <andrew.kenworthy@stackable.de>
2023-01-23 11:48:52 +0100

    release 23.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,8 @@ All notable changes to this project will be documented in this file.
 
 ## [Unreleased]
 
+## [23.1.0] - 2023-01-23
+
 ### Added
 
 - Log aggregation added ([#290]).
--- docs/modules/getting_started/examples/code/getting_started.sh
+++ docs/modules/getting_started/examples/code/getting_started.sh
@@ -16,26 +16,26 @@ fi
 
 case "$1" in
 "helm")
-echo "Adding 'stackable-dev' Helm Chart repository"
+echo "Adding 'stackable-stable' Helm Chart repository"
 # tag::helm-add-repo[]
-helm repo add stackable-dev https://repo.stackable.tech/repository/helm-dev/
+helm repo add stackable-stable https://repo.stackable.tech/repository/helm-stable/
 # end::helm-add-repo[]
 echo "Installing Operators with Helm"
 # tag::helm-install-operators[]
-helm install --wait zookeeper-operator stackable-dev/zookeeper-operator --version 0.13.0-nightly
-helm install --wait hdfs-operator stackable-dev/hdfs-operator --version 0.7.0-nightly
-helm install --wait commons-operator stackable-dev/commons-operator --version 0.5.0-nightly
-helm install --wait secret-operator stackable-dev/secret-operator --version 0.7.0-nightly
+helm install --wait zookeeper-operator stackable-stable/zookeeper-operator --version 23.1.0
+helm install --wait hdfs-operator stackable-stable/hdfs-operator --version 23.1.0
+helm install --wait commons-operator stackable-stable/commons-operator --version 23.1.0
+helm install --wait secret-operator stackable-stable/secret-operator --version 23.1.0
 # end::helm-install-operators[]
 ;;
 "stackablectl")
 echo "installing Operators with stackablectl"
 # tag::stackablectl-install-operators[]
 stackablectl operator install \
-  commons=0.5.0-nightly \
-  secret=0.7.0-nightly \
-  zookeeper=0.13.0-nightly \
-  hdfs=0.7.0-nightly
+  commons=23.1.0 \
+  secret=23.1.0 \
+  zookeeper=23.1.0 \
+  hdfs=23.1.0
 # end::stackablectl-install-operators[]
 ;;
 *)
