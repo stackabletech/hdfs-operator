87e8b26eb740f1cd94ad0d55693716315d389039
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2023-05-16 08:23:43 +0200

    Update rust/operator/src/container.rs
    Co-authored-by: Natalie <teo@nullable.se>

--- rust/operator/src/container.rs
+++ rust/operator/src/container.rs
@@ -937,8 +937,8 @@ impl ContainerConfig {
                     );
                 }
 
-                if let Some(Some(Some(memory_limit))) =
-                    resources.map(|r| r.limits.as_ref().map(|limits| limits.get("memory")))
+                if let Some(memory_limit) =
+                    resources.and_then(|r| r.limits.as_ref()?.get("memory"))
                 {
                     let memory_limit =
                         MemoryQuantity::try_from(memory_limit).with_context(|_| {
