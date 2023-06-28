78656343d32e253df0da680132c15f7c3dd9ac75
Malte Sander <contact@maltesander.com>
2023-06-28 16:25:58 +0200

    Update rust/operator/src/container.rs
    Co-authored-by: Sebastian Bernauer <sebastian.bernauer@stackable.de>

--- rust/operator/src/container.rs
+++ rust/operator/src/container.rs
@@ -748,6 +748,8 @@ impl ContainerConfig {
             ContainerConfig::Hdfs { role, .. } if role != &HdfsRole::DataNode => {
                 merged_config.resources().map(|c| c.into())
             }
+            // data node resources
+            ContainerConfig::Hdfs { .. } => merged_config.data_node_resources().map(|c| c.into()),
             // init containers inherit their respective main (app) container resources
             ContainerConfig::FormatNameNodes { .. } | ContainerConfig::FormatZooKeeper { .. } => {
                 merged_config.resources().map(|c| c.into())
@@ -761,15 +763,11 @@ impl ContainerConfig {
                     .with_memory_limit("512Mi")
                     .build(),
             ),
-            // data node resources
-            ContainerConfig::Hdfs { role, .. } if role == &HdfsRole::DataNode => {
-                merged_config.data_node_resources().map(|c| c.into())
-            }
             // data node init container inherit their respective main (app) container resources
             ContainerConfig::WaitForNameNodes { .. } => {
                 merged_config.data_node_resources().map(|c| c.into())
             }
-            _ => None,
+        }
         }
     }
 
