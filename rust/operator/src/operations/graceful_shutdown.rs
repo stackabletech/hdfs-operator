2f7e46f0961b48a8ceec5918b15a894b0be3a62b
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2023-10-16 08:43:37 +0200

    improve docs

--- rust/operator/src/operations/graceful_shutdown.rs
+++ rust/operator/src/operations/graceful_shutdown.rs
@@ -14,7 +14,8 @@ pub fn add_graceful_shutdown_config(
     merged_config: &(dyn MergedConfig + Send + 'static),
     pod_builder: &mut PodBuilder,
 ) -> Result<(), Error> {
-    // This must be always set by the merge mechanism, as we provide a default value.
+    // This must be always set by the merge mechanism, as we provide a default value,
+    // users can not disable graceful shutdown.
     if let Some(graceful_shutdown_timeout) = merged_config.graceful_shutdown_timeout() {
         pod_builder.termination_grace_period(graceful_shutdown_timeout)?;
     }
