b78d5594f2e41f2ebbe91ccdd32fa48118013379
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2023-10-18 14:52:10 +0200

    set error context

--- rust/operator/src/operations/graceful_shutdown.rs
+++ rust/operator/src/operations/graceful_shutdown.rs
@@ -1,10 +1,10 @@
-use snafu::Snafu;
+use snafu::{ResultExt, Snafu};
 use stackable_hdfs_crd::MergedConfig;
 use stackable_operator::builder::PodBuilder;
 
 #[derive(Debug, Snafu)]
 pub enum Error {
-    #[snafu(display("Failed to set terminationGracePeriod"), context(false))]
+    #[snafu(display("Failed to set terminationGracePeriod"))]
     SetTerminationGracePeriod {
         source: stackable_operator::builder::pod::Error,
     },
@@ -17,7 +17,9 @@ pub fn add_graceful_shutdown_config(
     // This must be always set by the merge mechanism, as we provide a default value,
     // users can not disable graceful shutdown.
     if let Some(graceful_shutdown_timeout) = merged_config.graceful_shutdown_timeout() {
-        pod_builder.termination_grace_period(graceful_shutdown_timeout)?;
+        pod_builder
+            .termination_grace_period(graceful_shutdown_timeout)
+            .context(SetTerminationGracePeriodSnafu)?;
     }
 
     Ok(())
