97b3b4db96d9d0bac500a05f4658f8fa973fe951
Andrew Kenworthy <andrew.kenworthy@stackable.de>
2025-01-10 14:33:59 +0100

    release 24.11.1
    Signed-off-by: Andrew Kenworthy <andrew.kenworthy@stackable.de>

--- docs/modules/hdfs/examples/getting_started/getting_started.sh
+++ docs/modules/hdfs/examples/getting_started/getting_started.sh
@@ -29,22 +29,22 @@ echo "Updating Helm repo"
 helm repo update
 echo "Installing Operators with Helm"
 # tag::helm-install-operators[]
-helm install --wait zookeeper-operator stackable-stable/zookeeper-operator --version 24.11.1-rc2
-helm install --wait hdfs-operator stackable-stable/hdfs-operator --version 24.11.1-rc2
-helm install --wait commons-operator stackable-stable/commons-operator --version 24.11.1-rc2
-helm install --wait secret-operator stackable-stable/secret-operator --version 24.11.1-rc2
-helm install --wait listener-operator stackable-stable/listener-operator --version 24.11.1-rc2
+helm install --wait zookeeper-operator stackable-stable/zookeeper-operator --version 24.11.1
+helm install --wait hdfs-operator stackable-stable/hdfs-operator --version 24.11.1
+helm install --wait commons-operator stackable-stable/commons-operator --version 24.11.1
+helm install --wait secret-operator stackable-stable/secret-operator --version 24.11.1
+helm install --wait listener-operator stackable-stable/listener-operator --version 24.11.1
 # end::helm-install-operators[]
 ;;
 "stackablectl")
 echo "installing Operators with stackablectl"
 # tag::stackablectl-install-operators[]
 stackablectl operator install \
-  commons=24.11.1-rc2 \
-  secret=24.11.1-rc2 \
-  listener=24.11.1-rc2 \
-  zookeeper=24.11.1-rc2 \
-  hdfs=24.11.1-rc2
+  commons=24.11.1 \
+  secret=24.11.1 \
+  listener=24.11.1 \
+  zookeeper=24.11.1 \
+  hdfs=24.11.1
 # end::stackablectl-install-operators[]
 ;;
 *)
