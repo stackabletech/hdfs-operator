9bc3d3bf9cb51a6b7f0bdec817c9971eed01df6c
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2025-06-16 13:33:26 +0200

    review feedback

--- tests/templates/kuttl/smoke/test_prometheus_metrics.py
+++ tests/templates/kuttl/smoke/test_prometheus_metrics.py
@@ -1,7 +1,6 @@
 # Fetch metrics from the built-in Prometheus endpoint of HDFS components.
 
 import logging
-import re
 import sys
 
 import requests
@@ -16,8 +15,13 @@ def check_metrics(
     )
     assert response.ok, "Requesting metrics failed"
 
+    # Split the response into lines to check for metric names at the beginning of each line.
+    # This is a bit slower than using a regex but it allows to use special characters like "{}" in metric names
+    # without needing to escape them.
+    response_lines = response.text.splitlines()
     for metric in expected_metrics:
-        assert re.search(f"^{metric}", response.text, re.MULTILINE) is not None, (
+        # Use any() with a generator to stop early if the metric is found.
+        assert any((line.startswith(metric) for line in response_lines)) is True, (
             f"Metric '{metric}' not found for {role}"
         )
 
@@ -30,9 +34,9 @@ def check_namenode_metrics(
         # Kind "MetricsSystem"
         'metrics_system_num_active_sources{context="metricssystem",hostname="hdfs-namenode-default-0"}',
         # Counter suffixed with "_total"
-        # The metric attributes can change so use .* for them.
+        # The metric attributes can change so we remove them from the expected metric.
         # The full name looks like: 'fs_namesystem_files_total{context="dfs",enabledecpolicies="RS-6-3-1024k",hastate="active",totalsynctimes="4 7 ",hostname="hdfs-namenode-default-0"}',
-        "fs_namesystem_files_total.*",
+        "fs_namesystem_files_total",
         # Metric suffixed with "_created"
         'namenode_files_created{processname="NameNode",sessionid="null",context="dfs",hostname="hdfs-namenode-default-0"}',
         # Boolean metric
@@ -53,17 +57,17 @@ def check_datanode_metrics(
         'metrics_system_num_active_sources{context="metricssystem",hostname="hdfs-datanode-default-0"}',
         # Kind "FSDatasetState" suffixed with "_total"
         # 'org_apache_hadoop_hdfs_server_datanode_fsdataset_impl_fs_dataset_impl_estimated_capacity_lost_total{context="FSDatasetState",storageinfo="FSDataset{dirpath=\'[/stackable/data/hdd/datanode,/stackable/data/hdd-1/datanode, /stackable/data/ssd/datanode]\'}",hostname="hdfs-datanode-default-0"}',
-        "org_apache_hadoop_hdfs_server_datanode_fsdataset_impl_fs_dataset_impl_estimated_capacity_lost_total.*",
+        "org_apache_hadoop_hdfs_server_datanode_fsdataset_impl_fs_dataset_impl_estimated_capacity_lost_total",
         # Kind "FSDatasetState"
         # 'org_apache_hadoop_hdfs_server_datanode_fsdataset_impl_fs_dataset_impl_capacity{context="FSDatasetState",storageinfo="FSDataset{dirpath=\'[/stackable/data/hdd/datanode, /stackable/data/hdd-1/datanode, /stackable/data/ssd/datanode]\'}",hostname="hdfs-datanode-default-0"}',
-        "org_apache_hadoop_hdfs_server_datanode_fsdataset_impl_fs_dataset_impl_capacity.*",
+        "org_apache_hadoop_hdfs_server_datanode_fsdataset_impl_fs_dataset_impl_capacity",
         # Kind "DataNodeActivity" suffixed with "_info"
         'datanode_blocks_get_local_path_info{sessionid="null",context="dfs",hostname="hdfs-datanode-default-0"}',
         # Kind "DataNodeActivity"
         'datanode_blocks_read{sessionid="null",context="dfs",hostname="hdfs-datanode-default-0"}',
         # Counter suffixed with "_total"
         # 'org_apache_hadoop_hdfs_server_datanode_fsdataset_impl_fs_dataset_impl_estimated_capacity_lost_total{context="FSDatasetState",storageinfo="FSDataset{dirpath=\'[/stackable/data/hdd/datanode,/stackable/data/hdd-1/datanode, /stackable/data/ssd/datanode]\'}",hostname="hdfs-datanode-default-0"}',
-        "org_apache_hadoop_hdfs_server_datanode_fsdataset_impl_fs_dataset_impl_estimated_capacity_lost_total.*",
+        "org_apache_hadoop_hdfs_server_datanode_fsdataset_impl_fs_dataset_impl_estimated_capacity_lost_total",
         # Boolean metric
         #'hadoop_datanode_security_enabled{kind="DataNodeInfo",role="DataNode",service="HDFS"}',
         # Non-special metric
