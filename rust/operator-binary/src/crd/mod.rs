fa97b864a2f1bff036914e9cb901fc3286810bc4
Stacky McStackface <95074132+stackable-bot@users.noreply.github.com>
2025-10-27 11:48:59 +0100

    chore(template): Updates for SDP 25.11.0 (#720)
    * chore: Generated commit to update templated files since the last template run up to stackabletech/operator-templating@e569102f2cb04490805bfa485d3e59d4736b18a4
    
    Reference-to: stackabletech/operator-templating@e569102 (Rollout pre-release updates for SDP 25.11.0)
    
    * chore: Fix Clippy's mismatched_lifetime_syntaxes lint
    
    ---------
    
    Co-authored-by: Techassi <git@techassi.dev>

--- rust/operator-binary/src/crd/mod.rs
+++ rust/operator-binary/src/crd/mod.rs
@@ -848,7 +848,7 @@ impl AnyNodeConfig {
 
     // Logging config is distinct between each role, due to the different enum types,
     // so provide helpers for containers that are common between all roles.
-    pub fn hdfs_logging(&self) -> Cow<ContainerLogConfig> {
+    pub fn hdfs_logging(&'_ self) -> Cow<'_, ContainerLogConfig> {
         match self {
             AnyNodeConfig::Name(node) => node.logging.for_container(&NameNodeContainer::Hdfs),
             AnyNodeConfig::Data(node) => node.logging.for_container(&DataNodeContainer::Hdfs),
@@ -856,7 +856,7 @@ impl AnyNodeConfig {
         }
     }
 
-    pub fn vector_logging(&self) -> Cow<ContainerLogConfig> {
+    pub fn vector_logging(&'_ self) -> Cow<'_, ContainerLogConfig> {
         match &self {
             AnyNodeConfig::Name(node) => node.logging.for_container(&NameNodeContainer::Vector),
             AnyNodeConfig::Data(node) => node.logging.for_container(&DataNodeContainer::Vector),
@@ -1080,7 +1080,7 @@ pub struct HdfsPodRef {
 }
 
 impl HdfsPodRef {
-    pub fn fqdn(&self, cluster_info: &KubernetesClusterInfo) -> Cow<str> {
+    pub fn fqdn(&'_ self, cluster_info: &KubernetesClusterInfo) -> Cow<'_, str> {
         self.fqdn_override.as_deref().map_or_else(
             || {
                 Cow::Owned(format!(
