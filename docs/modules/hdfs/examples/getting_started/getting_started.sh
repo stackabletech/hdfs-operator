c2c8de2689ae6771951d8740a5802d875645dd80
Techassi <git@techassi.dev>
2025-11-07 10:35:09 +0100

    chore: Release 25.11.0 (#729)
    Signed-off-by: Techassi <git@techassi.dev>

--- docs/modules/hdfs/examples/getting_started/getting_started.sh
+++ docs/modules/hdfs/examples/getting_started/getting_started.sh
@@ -21,22 +21,22 @@ case "$1" in
 "helm")
 echo "Installing Operators with Helm"
 # tag::helm-install-operators[]
-helm install --wait zookeeper-operator oci://oci.stackable.tech/sdp-charts/zookeeper-operator --version 25.11.0-rc1
-helm install --wait hdfs-operator oci://oci.stackable.tech/sdp-charts/hdfs-operator --version 25.11.0-rc1
-helm install --wait commons-operator oci://oci.stackable.tech/sdp-charts/commons-operator --version 25.11.0-rc1
-helm install --wait secret-operator oci://oci.stackable.tech/sdp-charts/secret-operator --version 25.11.0-rc1
-helm install --wait listener-operator oci://oci.stackable.tech/sdp-charts/listener-operator --version 25.11.0-rc1 --set preset=stable-nodes
+helm install --wait zookeeper-operator oci://oci.stackable.tech/sdp-charts/zookeeper-operator --version 25.11.0
+helm install --wait hdfs-operator oci://oci.stackable.tech/sdp-charts/hdfs-operator --version 25.11.0
+helm install --wait commons-operator oci://oci.stackable.tech/sdp-charts/commons-operator --version 25.11.0
+helm install --wait secret-operator oci://oci.stackable.tech/sdp-charts/secret-operator --version 25.11.0
+helm install --wait listener-operator oci://oci.stackable.tech/sdp-charts/listener-operator --version 25.11.0 --set preset=stable-nodes
 # end::helm-install-operators[]
 ;;
 "stackablectl")
 echo "installing Operators with stackablectl"
 # tag::stackablectl-install-operators[]
 stackablectl operator install \
-  commons=25.11.0-rc1 \
-  secret=25.11.0-rc1 \
-  listener=25.11.0-rc1 \
-  zookeeper=25.11.0-rc1 \
-  hdfs=25.11.0-rc1
+  commons=25.11.0 \
+  secret=25.11.0 \
+  listener=25.11.0 \
+  zookeeper=25.11.0 \
+  hdfs=25.11.0
 # end::stackablectl-install-operators[]
 ;;
 *)
