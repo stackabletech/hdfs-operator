992617235442d50f0bbc240f749aa1a05974a929
Nick <10092581+NickLarsenNZ@users.noreply.github.com>
2026-03-16 14:34:04 +0100

    chore: Release 26.3.0 (#767)
    Signed-off-by: Nick Larsen <nick.larsen@stackable.tech>

--- docs/modules/hdfs/examples/getting_started/getting_started.sh
+++ docs/modules/hdfs/examples/getting_started/getting_started.sh
@@ -24,22 +24,22 @@ case "$1" in
 "helm")
 echo "Installing Operators with Helm"
 # tag::helm-install-operators[]
-helm install --wait zookeeper-operator oci://oci.stackable.tech/sdp-charts/zookeeper-operator --version 26.3.0-rc1
-helm install --wait hdfs-operator oci://oci.stackable.tech/sdp-charts/hdfs-operator --version 26.3.0-rc1
-helm install --wait commons-operator oci://oci.stackable.tech/sdp-charts/commons-operator --version 26.3.0-rc1
-helm install --wait secret-operator oci://oci.stackable.tech/sdp-charts/secret-operator --version 26.3.0-rc1
-helm install --wait listener-operator oci://oci.stackable.tech/sdp-charts/listener-operator --version 26.3.0-rc1 --set preset=stable-nodes
+helm install --wait zookeeper-operator oci://oci.stackable.tech/sdp-charts/zookeeper-operator --version 26.3.0
+helm install --wait hdfs-operator oci://oci.stackable.tech/sdp-charts/hdfs-operator --version 26.3.0
+helm install --wait commons-operator oci://oci.stackable.tech/sdp-charts/commons-operator --version 26.3.0
+helm install --wait secret-operator oci://oci.stackable.tech/sdp-charts/secret-operator --version 26.3.0
+helm install --wait listener-operator oci://oci.stackable.tech/sdp-charts/listener-operator --version 26.3.0 --set preset=stable-nodes
 # end::helm-install-operators[]
 ;;
 "stackablectl")
 echo "installing Operators with stackablectl"
 # tag::stackablectl-install-operators[]
 stackablectl operator install \
-  commons=26.3.0-rc1 \
-  secret=26.3.0-rc1 \
-  listener=26.3.0-rc1 \
-  zookeeper=26.3.0-rc1 \
-  hdfs=26.3.0-rc1
+  commons=26.3.0 \
+  secret=26.3.0 \
+  listener=26.3.0 \
+  zookeeper=26.3.0 \
+  hdfs=26.3.0
 # end::stackablectl-install-operators[]
 ;;
 *)
