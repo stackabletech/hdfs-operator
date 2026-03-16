a8e9aeb2b1cd54f2ff535bb94a1947098bf6afc9
Nick Larsen <nick.larsen@stackable.tech>
2026-03-16 14:16:10 +0100

    chore: Release 26.3.0
    Signed-off-by: Nick Larsen <nick.larsen@stackable.tech>

--- Cargo.nix
+++ Cargo.nix
@@ -9519,7 +9519,7 @@ rec {
       };
       "stackable-hdfs-operator" = rec {
         crateName = "stackable-hdfs-operator";
-        version = "26.3.0-rc1";
+        version = "26.3.0";
         edition = "2021";
         crateBin = [
           {
